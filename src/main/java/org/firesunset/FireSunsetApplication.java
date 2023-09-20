3d3e63714d6ee9c5d1487c9ca545139af005a123
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-09-20 13:33:23 +0600

    Initial Commit

---
+++ src/main/java/org/firesunset/FireSunsetApplication.java
@@ -0,0 +1,13 @@
+package org.firesunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireSunsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireSunsetApplication.class, args);
+    }
+
+}
